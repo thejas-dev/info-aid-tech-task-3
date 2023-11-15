99fb6980ab2bec025399960a36487b007d05d685
thejas-dev <thejasgamerx123@gmail.com>
2023-11-15 14:32:07 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from 'react';
 
 function App() {
+
+
+  useEffect(()=>{
+    let celsius =document.getElementById('celsius'); 
+    let fahrenheit =document.getElementById('fahrenheit'); 
+    let kelvin =document.getElementById('kelvin'); 
+    celsius.oninput =function() 
+    { 
+        let f =(parseFloat(celsius.value)*9)/5+32; 
+        fahrenheit.value =parseFloat(f.toFixed(2)); 
+        let k =(parseFloat(celsius.value)+273.15); 
+        kelvin.value=parseFloat(k.toFixed(2)); 
+    } 
+    fahrenheit.oninput =function() 
+    { 
+        let c =((parseFloat(fahrenheit.value)-32)*5)/9; 
+        celsius.value=parseFloat(c.toFixed(2)); 
+        let k =(parseFloat(fahrenheit.value)-32)*5/9+273.15; 
+        kelvin.value=parseFloat(k.toFixed(2)); 
+    } 
+    kelvin.oninput=function() 
+    { 
+        let f=(parseFloat(kelvin.value)-273.15)*9/5+32; 
+        fahrenheit.value=parseFloat(f.toFixed(2)); 
+        let c=(parseFloat(kelvin.value)- 273.15); 
+        celsius.value=parseFloat(c.toFixed(2)); 
+    } 
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <div class="converter-row"> 
+            <div class="col"> 
+                <label>Fahrenheit</label> 
+                <input type="number" id="fahrenheit"/> 
+            </div> 
+            <div class="col"> 
+                <label>Celsius</label> 
+                <input type="number" id="celsius"/> 
+            </div> 
+            <div class="col"> 
+                <label>Kelvin</label> 
+                <input type="number" id="kelvin"/> 
+            </div> 
+        </div> 
+    </div> 
     </div>
   );
 }
